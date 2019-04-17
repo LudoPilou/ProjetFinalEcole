92b514ddce1b69645d9da65565615ac44634fe4a
IN-NN-013 <IN-NN-013@192.168.1.134>
2019-04-17 15:44:54 +0200

    commit Noter

--- college-serveur/src/main/java/org/college/serveur/dao/NoterDAO.java
+++ college-serveur/src/main/java/org/college/serveur/dao/NoterDAO.java
@@ -1,18 +1,13 @@
 package org.college.serveur.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.college.serveur.entities.Noter;
 import org.hibernate.Query;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 
 
 @Repository("daoNoter")
@@ -24,49 +19,48 @@ public class NoterDAO implements INoterDAO {
 	
 	
 	public NoterDAO() {
-	
+		
 	}
 
 
 
 	public void ajouter(Noter t) {
-		// TODO Auto-generated method stub
+		session.getCurrentSession().merge(t);
 		
 	}
 
 
 
 	public void modifier(Noter t) {
-		// TODO Auto-generated method stub
+		session.getCurrentSession().update(t);
 		
 	}
 
 
 
 	public void supprimer(Noter t) {
-		// TODO Auto-generated method stub
+		session.getCurrentSession().delete(t);
 		
 	}
 
 
 
 	public List<Noter> afficher() {
-		// TODO Auto-generated method stub
-		return null;
+		return session.getCurrentSession().createQuery("select n from Noter n").list();
 	}
 
 
 
 	public Noter getById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return (Noter) session.getCurrentSession().get(Noter.class, id);
 	}
 
 
 
 	public double getMoyenneParMatiere(int idMatiere) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		Query q=session.getCurrentSession().createQuery(" select avg(note) from Noter n where n.matiere.idMatiere =:id group by n.matiere.idMatiere").setParameter("id", idMatiere);
+		return (Double) q.list().get(0);
 	}
 
 	
